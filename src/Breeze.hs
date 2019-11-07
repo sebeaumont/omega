111a3dabf008e92e05f5b872f1e4dbacf9422362
Simon Beaumont <datalligator@icloud.com>
2019-11-07 21:31:00 +0000

    Bi-paramteric SparseVectors indexTyp valueTyp

--- src/Breeze.hs
+++ src/Breeze.hs
@@ -50,27 +50,23 @@ getRandomList n d = liftEntropy $ Entropy $ do
   replicateM n (Random.uniformR (0,d-1) g)
 
 
-
 -- | Sparse vectors have dimension and a list of indexes but no values yet, but could be pairs
 -- with the indexes.
-
+{-
 --data SparseVector a = SVec a (SortedList a) deriving (Show)
 data SparseVector a = SVec { dim_ :: !a
                            , idx_ :: !(SL.SortedList a)
                            } deriving (Show)
-
--- with values...
-data SparseVector2 i v = SVec2 { dim2_ :: !i
-                               , idx2_ :: !(SL.SortedList (i,v))
-                               } deriving (Show)
-
--- sparse distirbuted vectors have a uniform probaility p of an index at given value of dim_
-
-{-
-instance Functor SortedList where
-  fmap = Data.SortedList.map
 -}
 
+-- with values...
+data SparseVector i v = SVec { dim2_ :: !i
+                             , idx2_ :: !(SL.SortedList (i,v))
+                             }
+                      | BVec { dim_ :: !i
+                             , idx_ :: !(SL.SortedList i)
+                             }
+                      deriving (Show)
 
 
 toSortedPairs :: (Ord a, Ord b) => [a] -> [b] -> SL.SortedList (a, b)
@@ -83,44 +79,62 @@ toZeros :: (Ord i, Ord v, Num v) => [i] -> SL.SortedList (i, v)
 toZeros i = toSortedPairs i (repeat 0)
 
 
--- These are the public intefaces...
+-- sparse distirbuted vectors have a uniform probaility p of an index at given value of dim_
+-- | p d useful for bit vectors existential on index but with no values
 
--- | Sparse random vectors
-
-makeSparseRandomVector  :: (MonadEntropy m, Random.Variate a, Integral a) =>
-                     Int -> a -> m (SparseVector a)
-makeSparseRandomVector p d = (SVec d . SL.toSortedList) <$> (getRandomList p d)
-
-
-makeSparseRandomVector2  :: (MonadEntropy m, Random.Variate a, Integral a, Num v, Ord v) =>
-                      Int -> a -> m (SparseVector2 a v)
-makeSparseRandomVector2 p d = (SVec2 d . toZeros) <$> (getRandomList p d)
+makeSparseRandomBitVector  :: (MonadEntropy m, Random.Variate a, Integral a) =>
+                              Int -> a -> m (SparseVector a v)
+makeSparseRandomBitVector p d = (BVec d . SL.toSortedList) <$> (getRandomList p d)
 
 
 -- | Could make this a vector of vectors... (matrix) rather than a list
-makeSparseRandomVectors :: (MonadEntropy m, Random.Variate a, Integral a) =>
-                     Int -> Int -> a -> m [SparseVector a]
+-- withEntropy $ makeSparseRandomBitVectors 10 16 16496 :: IO ([SparseVector Word16 Void])
+
+makeSparseRandomBitVectors :: (MonadEntropy m, Random.Variate a, Integral a) =>
+                              Int -> Int -> a -> m [SparseVector a v]
+makeSparseRandomBitVectors n p d = replicateM n (makeSparseRandomBitVector p d)
+
+-- | p d v=0 sparse random with values...
+-- withEntropy $ makeSparseVector2 16 16392 :: IO (SparseVector2 Word16 Double)
+
+makeSparseRandomVector  :: (MonadEntropy m, Random.Variate a, Integral a, Num v, Ord v) =>
+                           Int -> a -> m (SparseVector a v)
+makeSparseRandomVector p d = (SVec d . toZeros) <$> (getRandomList p d)
+
+-- | Could make this a vector of vectors... (matrix) rather than a list
+makeSparseRandomVectors :: (MonadEntropy m, Random.Variate a, Integral a, Num v, Ord v) =>
+                              Int -> Int -> a -> m [SparseVector a v]
 makeSparseRandomVectors n p d = replicateM n (makeSparseRandomVector p d)
 
 
--- XXX shoudn't Ord/Integral constraint be on SparseVector type?
--- for which we'd need a GADT e.g. is this a good idea?
-{-
-data SparseVector where
-  SVec :: Integral a => !a -> !SortedList a
--} 
-
 -- | Add two sparse vectors
 -- XXX no dim check, no dependent types.
- 
-add :: Ord a => SparseVector a -> SparseVector a -> SparseVector a
-add (SVec !ud !ui) (SVec !vd !vi) = SVec ud (SL.union ui vi)
 
+add :: (Ord i, Ord v, Num v) => SparseVector i v -> SparseVector i v -> SparseVector i v
+add (BVec !ud !ui) (BVec !vd !vi) = BVec ud (SL.union ui vi)
+add (SVec !ud !ui) (SVec !vd !vi) =
+  let us = SL.fromSortedList ui
+      vs = SL.fromSortedList vi
+  in SVec ud (SL.toSortedList (unionAdd us vs))
+
+  
+-- | Trixy little pattern match to merge (add) sorted index pairs
+unionAdd :: (Ord a, Num b) => [(a, b)] -> [(a, b)] -> [(a, b)]
+unionAdd p1@((i1,v1):r1) p2@((i2,v2):r2)
+  | i1 > i2 = (i2, v2) : unionAdd p1 r2
+  | i1 < i2 = (i1, v1) : unionAdd r1 p2
+  | i1 == i2 = (i1, v1+v2) : unionAdd r1 r2
+unionAdd [] l@(x:xs) = l  
+unionAdd l@(x:xs) [] = l
+unionAdd [] [] = []
+
+-- does it make sense to add mixed?
+{-
 size :: SparseVector a -> Int
 size (SVec _ !ui) = length ui
 
 density :: Integral a => SparseVector a -> Double
 density v@(SVec !ud _) = fromIntegral (size v) / fromIntegral ud
-
+-}
 
 
