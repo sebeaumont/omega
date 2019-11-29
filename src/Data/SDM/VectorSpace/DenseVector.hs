7a9b103d0ea5b252de4e4d57af50c49eeecaae6b
Simon Beaumont <datalligator@icloud.com>
2019-11-29 15:35:04 +0000

    Start to make densification work.

--- src/Data/SDM/VectorSpace/DenseVector.hs
+++ src/Data/SDM/VectorSpace/DenseVector.hs
@@ -21,20 +21,44 @@ bitsPerWord = round $ logBase 2 (fromIntegral $ (maxBound :: Word))
 wordBit :: Integral a => a -> (a, a)
 wordBit n = divMod n bitsPerWord
 
--- | Your mission Jim... since we have to index the `U.Vector` using
--- `Int` as this is given in its type unless we can convert other
--- Integral types to Int freely? then the type of `a` is inferred to
--- be Int and we lose the polymorphism even though there is no real
--- connection other than computing the index in the target `U.Vector`
-
+{-# INLINE idxFoo #-}
+-- this doesn't actually work yet as we may need to merge idx with (+) 
 idxFoo :: Integral b => SparseBitVector -> (Int, [(Int, b)])
 idxFoo bv =
   let foo (i,b) = (i, fromIntegral $ 2^b)
       maxi = (Set.findMax $ index bv) `div` bitsPerWord
-  in (maxi, map foo [wordBit i | i <- Set.toList $ index bv]) where
+      idx = [wordBit i | i <- Set.toList $ index bv]
+  in (maxi, map foo idx) where
 
 bitVecToDense :: (Integral a, U.Unbox a) => SparseBitVector -> U.Vector a
 bitVecToDense bv =
   let (size, wib) = idxFoo bv
       vector = U.replicate (fromIntegral (size+1)) 0
   in vector U.// wib
+
+
+class Densify a where
+  toDense:: a -> U.Vector Word
+  
+instance Densify SparseBitVector where
+  toDense = bitVecToDense
+  
+  
+-- | Incremental union/merge of sorted lists of index pairs takes
+-- binary function of values. N.B. due to non-greedy behaviour only
+-- applies binary fn to `snd` of matched `fst` in either list so this
+-- must be applied each time we wish to aggregate values.  So
+-- for duplicated `fst` or indexes e.g. we would get:
+--  `unionWith` (+) [(1,2),(1,3),(1,4)] [(1,2),(1,3),(1,4)] = [(1,4),(1,6),(1,8)]
+--  rather than [(1,18)]
+
+{-# INLINE unionWith #-}
+unionWith :: (Ord a, Num b) => (b -> b -> b) -> [(a, b)] -> [(a, b)] -> [(a, b)]
+unionWith f p1@((i1,v1):r1) p2@((i2,v2):r2)
+  | i1 > i2 = (i2, v2) : unionWith f p1 r2
+  | i1 < i2 = (i1, v1) : unionWith f r1 p2
+  | i1 == i2 = (i1, f v1 v2) : unionWith f r1 r2 
+unionWith _ l []  = l  
+unionWith _ [] l = l
+unionWith _ _ _ = []
+
