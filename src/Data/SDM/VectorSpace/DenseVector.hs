f0b435dc22bb4c86759b81e0a1c423ab7752b1b3
Simon Beaumont <datalligator@icloud.com>
2019-11-29 18:56:47 +0000

    index computation seems like it might work now

--- src/Data/SDM/VectorSpace/DenseVector.hs
+++ src/Data/SDM/VectorSpace/DenseVector.hs
@@ -1,8 +1,7 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Data.SDM.VectorSpace.DenseVector where
 
 import Data.Bits
-import Data.Word
-import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Data.Vector.Unboxed as U
 
@@ -23,18 +22,28 @@ wordBit n = divMod n bitsPerWord
 
 {-# INLINE idxFoo #-}
 -- this doesn't actually work yet as we may need to merge idx with (+) 
-idxFoo :: Integral b => SparseBitVector -> (Int, [(Int, b)])
+idxFoo :: (Bits b, Integral b) => SparseBitVector -> (Int, [(Int, b)])
 idxFoo bv =
   let foo (i,b) = (i, fromIntegral $ 2^b)
       maxi = (Set.findMax $ index bv) `div` bitsPerWord
       idx = [wordBit i | i <- Set.toList $ index bv]
-  in (maxi, map foo idx) where
+  in (maxi, mergeWith (.&.) (map foo idx)) where
 
-bitVecToDense :: (Integral a, U.Unbox a) => SparseBitVector -> U.Vector a
+    
+bitVecToDense :: (Bits a, Integral a, U.Unbox a) => SparseBitVector -> U.Vector a
 bitVecToDense bv =
   let (size, wib) = idxFoo bv
       vector = U.replicate (fromIntegral (size+1)) 0
   in vector U.// wib
+
+  
+{-# INLINE mergeWith #-}
+mergeWith :: Ord a => (b -> b -> b) -> [(a, b)] -> [(a, b)]
+mergeWith f ((i1,v1):(i2,v2):rs) 
+  | i1 == i2 = let m = (i1, f v1 v2) in mergeWith f (m:rs)
+  | otherwise = (i1,v1) : mergeWith f ((i2,v2):rs)
+mergeWith _ l@((_,_):[]) = l  
+mergeWith _ []  = []  
 
 
 class Densify a where
@@ -42,23 +51,3 @@ class Densify a where
   
 instance Densify SparseBitVector where
   toDense = bitVecToDense
-  
-  
--- | Incremental union/merge of sorted lists of index pairs takes
--- binary function of values. N.B. due to non-greedy behaviour only
--- applies binary fn to `snd` of matched `fst` in either list so this
--- must be applied each time we wish to aggregate values.  So
--- for duplicated `fst` or indexes e.g. we would get:
---  `unionWith` (+) [(1,2),(1,3),(1,4)] [(1,2),(1,3),(1,4)] = [(1,4),(1,6),(1,8)]
---  rather than [(1,18)]
-
-{-# INLINE unionWith #-}
-unionWith :: (Ord a, Num b) => (b -> b -> b) -> [(a, b)] -> [(a, b)] -> [(a, b)]
-unionWith f p1@((i1,v1):r1) p2@((i2,v2):r2)
-  | i1 > i2 = (i2, v2) : unionWith f p1 r2
-  | i1 < i2 = (i1, v1) : unionWith f r1 p2
-  | i1 == i2 = (i1, f v1 v2) : unionWith f r1 r2 
-unionWith _ l []  = l  
-unionWith _ [] l = l
-unionWith _ _ _ = []
-
