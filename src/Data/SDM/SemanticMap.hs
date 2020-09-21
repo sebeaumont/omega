06004f4851e206e1c5a14bd0c4ca4a2e618745ba
Simon Beaumont <datalligator@icloud.com>
2020-09-21 20:15:06 +0100

    Some module refcatoring and type generalisation

--- src/Data/SDM/SemanticMap.hs
+++ src/Data/SDM/SemanticMap.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE BangPatterns #-}
-module Data.SDM.Index where
+module Data.SDM.SemanticMap where
 
 import Control.Monad.IO.Class (liftIO)
 
@@ -12,13 +13,20 @@ import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy.IO as TIO
 import Data.Text.Tokenize
 import qualified Data.HashMap.Strict as Map
+import Control.DeepSeq (NFData)
+
+-- TODO rename both the module and this type and factor out the core map updating
+-- relevant function should be obvious from this newtype/c'tor usage below...
+
+-- | More powerful type for SemanticMap
+newtype SemanticMap a = SemanticMap (Map.HashMap a SemanticVector) deriving (NFData)
 
 -- | Map from term tokens to SemanticVectors   
-type TokenMap = Map.HashMap Text SemanticVector
+type TokenMap = SemanticMap Text
 
 -- | Update map with vector returning updated map
 updateVector :: TokenMap -> Text -> SemanticVector -> TokenMap
-updateVector m k v = Map.insert k v m
+updateVector (SemanticMap m) k v = SemanticMap $ Map.insert k v m
 
 updateVectors :: TokenMap -> [Text] -> [SemanticVector] -> TokenMap
 updateVectors m (k:ks) (v:vs) =
@@ -28,10 +36,10 @@ updateVectors m _ [] = m
 
 -- | Lookup token and return a SemanticVector either existing or new.
 ensureSemanticVector :: MonadEntropy m => TokenMap -> Text -> m (SemanticVector, TokenMap)
-ensureSemanticVector tm tok = 
+ensureSemanticVector (SemanticMap tm) tok = 
   case Map.lookup tok tm of
-    Nothing -> makeSemanticVector >>= \sv -> return (sv, Map.insert tok sv tm)
-    Just sv' -> return (sv', tm)
+    Nothing -> makeSemanticVector >>= \sv -> return (sv, SemanticMap $ Map.insert tok sv tm)
+    Just sv' -> return (sv', SemanticMap $ tm)
 
 -- | Frame based indexing making sure updated map is used
 frameVectors :: MonadEntropy m => TokenMap -> [Text] -> m [(SemanticVector, TokenMap)]
@@ -41,7 +49,7 @@ frameVectors tm (t:ts) = do
   return $! (v, tm') : rs
 frameVectors _ [] = return []
 
--- | Index a frame
+-- | SemanticMap a frame
 indexFrame :: MonadEntropy m => TokenMap -> [Text] -> m TokenMap
 indexFrame tm frame = do
   vectorMap <- frameVectors tm frame
@@ -51,7 +59,7 @@ indexFrame tm frame = do
       -- now update the vectors with the mutual info
   return $ updateVectors tm' frame mv 
 
--- | Index frames for side effects on TokenMap
+-- | SemanticMap frames for side effects on TokenMap
 indexFrames :: MonadEntropy m => TokenMap -> [[Text]] -> m TokenMap
 indexFrames tm (f:fs) = do
   tm' <- indexFrame tm f
@@ -59,31 +67,31 @@ indexFrames tm (f:fs) = do
   return $! rs
 indexFrames tm [] = return tm
 
--- | Index a text with given frame size and overlap
+-- | SemanticMap a text with given frame size and overlap
 indexText :: MonadEntropy m => Text -> Int -> Int -> m TokenMap
 indexText text fsize over =
   let fms = frames fsize over $ tokens text
-  in indexFrames Map.empty fms
+  in indexFrames (SemanticMap Map.empty) fms
 
 -- IO, IO it's off to work we go...
--- | e.g. Index tokenised text from stdin
+-- | e.g. SemanticMap tokenised text from stdin
 indexStdin :: Int -> Int -> IO TokenMap  
 indexStdin sz ov = 
   withEntropy $ do
     text <- liftIO TIO.getContents
     let fms = frames sz ov $ tokens text
-    indexFrames Map.empty fms
+    indexFrames (SemanticMap Map.empty) fms
 
 indexFile :: FilePath -> Int -> Int -> IO TokenMap
 indexFile fp sz ov =
   withEntropy $ do
     text <- liftIO $ TIO.readFile fp
     let fms = frames sz ov $ tokens text
-    indexFrames Map.empty fms
+    indexFrames (SemanticMap Map.empty) fms
     
 -- | Select nearest neighbours with difference (normalised distance) below given threshold
 neighbours :: TokenMap -> SemanticVector -> Int -> Int -> [(Text, Int)]
-neighbours !m !v !s !n =
+neighbours !(SemanticMap m) !v !s !n =
   let !v' = sV v
       !namedist = sortOn snd [(t, distance (sV u) v') | (t,u) <- Map.toList m]
   in
@@ -91,5 +99,4 @@ neighbours !m !v !s !n =
 
 -- | Lookup a token in the map
 token :: TokenMap -> Text -> Maybe SemanticVector
-token m s = Map.lookup s m
-
+token !(SemanticMap m) !s = Map.lookup s m
